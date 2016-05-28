1b1308f4f47dc9e5e4531aa2f753c534d1df3549
Jessica <jkustra92@gmail.com>
2016-05-28 11:10:14 -0400

    getting search form to connect to marvel's db. success.

--- controllers/comics.js
+++ controllers/comics.js
@@ -1,13 +1,12 @@
 var express = require("express");
 var router = express.Router();
 var md5 = require("md5");
+var request = require("request");
 var Comic = require("../models/comics.js")
 var publicKey = process.env.MARVEL_PUBLIC_KEY;
 var privateKey = process.env.MARVEL_PRIVATE_KEY;
 var ts = Date.now();
 var hash = md5(ts+privateKey+publicKey)
-
-//http://gateway.marvel.com:80/v1/public/comics?title=Hawkeye&startYear=2012&issueNumber=3&apikey=a7fe91c1a8f9dff79e43e342dfc46824
 
 //============
 //search form
@@ -24,21 +23,19 @@ router.post("/search", function(req, res){
   // console.log(searchTitle)
   // console.log(searchIssueNumber)
   // console.log(searchStartYear)
-  res.redirect("/comics/" + searchTitle + "/" + searchIssueNumber + "/" + searchStartYear);
+  res.redirect("/comics/" + searchTitle + "/" + searchStartYear + "/" + searchIssueNumber)
 })
 
-
-router.get("/:title/:issueNumber/:startYear", function(req, res){
+router.get("/:title/:startYear/:issueNumber", function(req, res){
   var title = req.params.title;
   var issueNumber = req.params.issueNumber;
   var startYear = req.params.startYear;
-  // console.log(title);
-  // console.log(issueNumber);
-  // console.log(startYear);
-  request("http://gateway.marvel.com/v1/public/comics?title=" + encodeURIComponent(title) + "&StartYear=" + startYear + "&issueNumber=" + issueNumber + "ts=" + ts + "apikey=" + publicKey + "hash=" + hash), function(err, response, body){
-    // console.log(response.body)
+  console.log(title, issueNumber, startYear);
+  // request("http://gateway.marvel.com/v1/public/comics?title=Hawkeye&startYear=2012&issueNumber=3&ts=1464445747&apikey=a7fe91c1a8f9dff79e43e342dfc46824&hash=e74f4e496dbd0b8dfa55d26cf2fc797f", function(err, response, body){
+  request("http://gateway.marvel.com/v1/public/comics?title=" + encodeURIComponent(title) + "&startYear=" + startYear + "&issueNumber=" + issueNumber + "&ts=" + ts + "&apikey=" + publicKey + "&hash=" + md5(ts+privateKey+publicKey), function(err, response, body){
+    console.log('g', response.body)
     res.send(response.body)
-  }
+  });
 });
 
 module.exports = router;
